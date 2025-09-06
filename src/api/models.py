69ae39bbefee85494fca0d853cd19c68f9bd51d0
David Betancourt <dmarquez.develop@gmail.com>
2025-09-06 19:21:53 +0000

    CRUD of chef done

--- src/api/models.py
+++ src/api/models.py
@@ -6,7 +6,7 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String(120), nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
@@ -17,3 +17,24 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Chef(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(nullable=False)
+    name: Mapped[str] =  mapped_column(String(120), unique=True, nullable=False)
+    rating: Mapped[int] = mapped_column(nullable=False)
+
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "name": self.name,
+            "rating": self.rating
+            # do not serialize the password, its a security breach
+        }
+        
+ 
+    
