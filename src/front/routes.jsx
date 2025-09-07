eab321954fb3903981598b94634727389a9118cb
David Betancourt <dmarquez.develop@gmail.com>
2025-09-07 18:28:46 +0000

    Front missing PUT method, DELETE, GET and POST done

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -9,6 +9,9 @@ import { Layout } from "./pages/Layout";
 import { Home } from "./pages/Home";
 import { Single } from "./pages/Single";
 import { Demo } from "./pages/Demo";
+import { Chef } from "./pages/Chef";
+import { SingleChef } from "./pages/SingleChef";
+import NewChef from "./pages/NewChef";
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
@@ -25,6 +28,10 @@ export const router = createBrowserRouter(
         <Route path= "/" element={<Home />} />
         <Route path="/single/:theId" element={ <Single />} />  {/* Dynamic route for single items */}
         <Route path="/demo" element={<Demo />} />
+
+        <Route path= "/chef" element={<Chef />} />
+        <Route path="/chef/:chef_id" element={ <SingleChef />} />
+        <Route path= "/add_chef" element={<NewChef />} />
       </Route>
     )
 );
