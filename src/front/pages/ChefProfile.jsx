93aaac9148eb1fad0941fd8c15b4190d00112fe7
David Betancourt <dmarquez.develop@gmail.com>
2025-09-21 01:23:52 +0000

    component UploadChefImage is having issues with the script in index.html

--- src/front/pages/ChefProfile.jsx
+++ src/front/pages/ChefProfile.jsx
@@ -1,25 +1,65 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
-import { ChefInfo } from "../components/ChefInfo";
+import { Link, useParams } from "react-router-dom"; 
+import UploadChefImage from "../components/UploadChefImage.jsx";
 
 
 const ChefProfile = () => {
 
+    const [chef, setChef] = useState([])
+
+    const { chef_id } = useParams() || {};
+
+    const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+    useEffect(() => {
+        getChefInfo()
+    }, []);
+
+    function getChefInfo() {
+        const token = localStorage.getItem("tokenChef")
+        fetch(backendUrl + `/api/chef_info`, {
+            headers: { 'Authorization': `Bearer ${token}` }
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                setChef(data)
+            }
+            )
+    }
+
+
     const { store, dispatch } = useGlobalReducer()
 
-    return(
+    return (
         <div>
-            {store.authChef ? 
-            <>
-            <ChefInfo/>
-            
-            
-            </>            
-            :
-            <h1>Tienes que autenticarte</h1>
+            {store.authChef ?
+                <>
+                    <div className="container text-center">
+                        {chef && chef.name ? (
+                            <>
+                                <h2>{chef.name}</h2>
+                                <p>Email: {chef.email}</p>
+                                <p>Rating: {chef.rating}</p>
+                                {/* Add more chef details here */}
+
+                                <h1>Chef Image</h1>
+                                <img src={"https://picsum.photos/200/300"} alt="chef_image" />
+
+                                <UploadChefImage/>
+
+                            </>
+                        ) : (
+                            <p>Loading chef info...</p>
+                        )}
+                    </div>
+                </>
+                :
+                <h1>Tienes que autenticarte</h1>
             }
         </div>
-        
+
     )
 }
 
