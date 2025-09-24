f9b6ee9246521d0643bbe370cc935e842a7668b3
Jezv95 <138320134+Jezv95@users.noreply.github.com>
2025-09-24 20:26:07 +0000

    aded missing things from revert

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -8,84 +8,111 @@ export const Navbar = () => {
 
 	const { store, dispatch } = useGlobalReducer()
 
-	function logout() {
+	function logoutChef() {
 		// limpiar token 
 		localStorage.removeItem("tokenChef")
 		// pasar auth a false
 		dispatch({ type: "set_auth_chef", payload: false })
 		// redireccioar a login
-		navigate("/login_chef")
+		navigate("/")
+
+	}
+
+	function logoutUser() {
+		// limpiar token 
+		localStorage.removeItem("tokenUser")
+		// pasar auth a false
+		dispatch({ type: "set_auth_user", payload: false })
+		// redireccioar a login
+		navigate("/")
+
+	}
+
+	function logoutAdmin() {
+		// limpiar token 
+		localStorage.removeItem("tokenAdmin")
+		// pasar auth a false
+		dispatch({ type: "set_auth_admin", payload: false })
+		// redireccioar a login
+		navigate("/")
 
 	}
 
 	return (
-		<nav className="navbar navbar-light bg-light">
-			{store.authChef ?
-				<>
-					<button className="btn btn-danger" onClick={logout}>Logout</button>
-					<Link to="/chef_profile">
-						<button className="btn btn-primary">Check Profile</button>
-					</Link>
-
-				</>
-				:
-				<>
+		<>
+			
+			
+		 
+			
+			<nav className="navbar navbar-light bg-light">
 					<div className="container">
-						<Link to="/">
-							<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-						</Link>
-						<div className="ml-auto">
-							<Link to="/demo">
-								<button className="btn btn-primary">Check the Context in action</button>
+
+						{store.authChef ? <button className="btn btn-danger" onClick={logoutChef}>Logout</button>: null}
+						{store.authUser ? <button className="btn btn-danger" onClick={logoutUser}>Logout</button>: null}
+						{store.authAdmin ? <button className="btn btn-danger" onClick={logoutAdmin}>Logout</button>: null}
+						
+			{store.authAdmin ?
+						 <div className="ml-auto">
+							<Link to="/testadm">
+								<button className="btn btn-primary">Home</button>
 							</Link>
-						</div>
-
-
-						<div className="ml-auto">
+						</div> :null}
+			{store.authChef ?
+						 <div className="ml-auto">
 							<Link to="/chefs">
-								<button className="btn btn-primary">Chefs</button>
+								<button className="btn btn-primary">Home</button>
 							</Link>
-						</div>
+						</div> :null}
+						{store.authUser || store.authAdmin ?
 						<div className="ml-auto">
 							<Link to="/recipes">
 								<button className="btn btn-primary">Recipes</button>
 							</Link>
-						</div>
+						</div>:null}
+						{store.authAdmin ?
 						<div className="ml-auto">
 							<Link to="/ingredientes">
-								<button className="btn btn-primary">Ingredientes</button>
+								<button className="btn btn-primary">Ingredients</button>
 							</Link>
-						</div>
+						</div>:null}
+						{store.authAdmin ?
 						<div className="ml-auto">
 							<Link to="/utensilios">
-								<button className="btn btn-danger">Utensilios</button>
+								<button className="btn btn-primary">Utensils</button>
 							</Link>
-						</div>
+						</div>:null}
+						{store.authAdmin ?
 						<div className="ml-auto">
 							<Link to="/adminuser">
 								<button className="btn btn-primary">Admins</button>
 							</Link>
-						</div>
+						</div>:null}
+						{ store.authUser || store.authAdmin || store.authChef?
 						<div className="ml-auto">
 							<Link to="/questions">
 								<button className="btn btn-primary">Questions</button>
 							</Link>
-						</div>
+						</div>:null}
+						{store.authUser || store.authAdmin || store.authChef?
 						<div className="ml-auto">
 							<Link to="/answers">
 								<button className="btn btn-primary">Answer</button>
 							</Link>
-						</div>
+						</div>:null}
+						{store.authUser || store.authAdmin || store.authChef?
 						<div className="ml-auto">
 							<Link to="/califications">
 								<button className="btn btn-primary">Califications</button>
 							</Link>
+						</div>:null}
+						{store.authChef ?
+						<div>
 							<Link to="/utensilio_receta">
 								<button className="btn btn-danger">Agregar utensilio a receta</button>
 							</Link>
-						</div>
+						</div>:null}
 					</div>
-
+					{ store.authUser ?
 					<div className="dropdown">
 						<button className="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
 							Fav Recipes <span className="badge text-bg-secondary"> {store.favItems.length} </span>
@@ -100,24 +127,27 @@ export const Navbar = () => {
 								</li>)}
 
 						</ul>
-					</div>
+					</div> :null}
+					{ store.authUser ?
 					<div className="ml-auto">
 						<Link to="/utensil_user">
 							<button className="btn btn-danger">utensilio usuario</button>
 						</Link>
-					</div>
+					</div>:null}
+					{ store.authUser ?
 					<div className="ml-auto">
 						<Link to="/ingredient_users">
 							<button className="btn btn-primary">Ingredient Users</button>
 						</Link>
-					</div>
+					</div>:null}
+					{ store.authUser ?
 					<div className="ml-auto">
 						<Link to="/users">
 							<button className="btn btn-danger">Agregar usuario</button>
 						</Link>
-					</div>
-				</>
-			}
-		</nav >
+					</div> : null}
+					
+					</nav >
+	</>
 	);
 };
