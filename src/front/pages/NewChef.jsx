f74e6e102b0df8c2e42f1187d5573f5e0c8e2e83
David Betancourt <dmarquez.develop@gmail.com>
2025-09-24 05:27:25 +0000

    added image tab to create new chef, however ChefSignUp is having issues with POST

--- src/front/pages/NewChef.jsx
+++ src/front/pages/NewChef.jsx
@@ -22,7 +22,7 @@ const NewChef = () => {
         console.log(name, email, password)
         const requestOptions = {
             method: 'POST',
-            headers: {"Content-Type": "application/json"},
+            headers: { "Content-Type": "application/json" },
             body: JSON.stringify(
                 {
                     "email": email,
