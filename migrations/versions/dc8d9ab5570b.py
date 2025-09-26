4812ba6349ce0b57946645e436005933e275c6a5
Anthony González <asgtoribio@gmail.com>
2025-09-26 04:08:04 +0000

    finalizado

---
+++ migrations/versions/dc8d9ab5570b.py
@@ -0,0 +1,24 @@
+"""Merge heads
+
+Revision ID: dc8d9ab5570b
+Revises: 
+Create Date: 2025-09-26 03:50:52.883047
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'dc8d9ab5570b'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
