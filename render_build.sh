8697ba0efb079e1ca38c2063624e1bff54698b2a
David Betancourt <dmarquez.develop@gmail.com>
2025-10-06 22:22:04 +0000

    deleted data from render_build

--- render_build.sh
+++ render_build.sh
@@ -5,27 +5,11 @@ set -o errexit
 npm install
 npm run build
 
-pip install
+pipenv install
 
-pip run upgrade
-chmod +x render_build.sh
+pipenv run upgrade
+
 
 flask insert-test-data
-flask insert-test-users
-flask insert-test-chefs
-flask insert-test-ingredients
-flask insert-test-utensils
 
-pip install --upgrade pip
-pip install psycopg2-binary==2.9.9
-pip install flask==2.3.3
-pip install flask-sqlalchemy==3.0.5
-pip install flask-migrate==4.0.5
-pip install flask-cors==4.0.0
-pip install flask-swagger==0.2.14
-pip install flask-admin==1.6.1
-pip install gunicorn==21.2.0
-pip install python-dotenv==1.0.0
-pip install alembic==1.12.1
-pip install pyyaml==6.0.1
-flask db upgrade
+
